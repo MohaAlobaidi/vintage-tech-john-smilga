37c8a89872a00d04f0578c35dc15144ed19487e8
MOHAMMED <mohammed@MOHAMMEDs-MacBook-Pro.local>
2020-08-16 22:18:35 +0200

    login strapi

--- src/strapi/loginUser.js
+++ src/strapi/loginUser.js
@@ -2,10 +2,11 @@ import axios from 'axios'
 import  url from './../utils/URL'
 //login user
 async function  loginUser({email,password}){
-const response = await axios.post(`${url}/auth/local/`,{
+const response = await axios.post(`${url}/auth/local`,{
   identifeir:email,
   password
-}).catch(error =>console.log(error) )
+})
+.catch(error =>console.log(error) )
 return response
 }
 export default loginUser
